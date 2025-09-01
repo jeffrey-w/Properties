451e7c5a5d7b47dea766aeaa07a3709fcea85342
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2025-09-01 15:24:22 -0400

    edit comments

--- Properties/src/Patches/Diff.cs
+++ Properties/src/Patches/Diff.cs
@@ -8,14 +8,18 @@ public static class Diff
 {
     /// <summary>
     /// Provides the <see cref="Patch" /> that defines the transformation from the
-    /// <paramref name="first" /> <see cref="IPropertyDictionary{T}" /> to the
-    /// <paramref name="second" />.
+    /// <paramref name="first" /> collection of <see cref="Property">properties</see>
+    /// to the <paramref name="second" />.
     /// </summary>
-    /// <param name="first">The source <see cref="IPropertyDictionary{T}" />.</param>
-    /// <param name="second">The target <see cref="IPropertyDictionary{T}" />.</param>
+    /// <param name="first">
+    /// The source collection of <see cref="Property">properties</see>.
+    /// </param>
+    /// <param name="second">
+    /// The target collection of <see cref="Property">properties</see>.
+    /// </param>
     /// <returns>
-    /// A <see cref="Patch" /> to obtain the <paramref name="second" />
-    /// <see cref="IPropertyDictionary{T}" /> from the <paramref name="first" />.
+    /// A <see cref="Patch" /> to obtain the <paramref name="second" /> collection of
+    /// <see cref="Property">properties</see> from the <paramref name="first" />.
     /// </returns>
     public static Patch ComputeFor(IEnumerable<Property> first, IEnumerable<Property> second)
     {
