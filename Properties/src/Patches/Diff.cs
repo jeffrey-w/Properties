9cc6a59f756d98ce42b50b82a5328b5144b51de5
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2025-09-01 15:22:22 -0400

    make `Diff` public

--- Properties/src/Patches/Diff.cs
+++ Properties/src/Patches/Diff.cs
@@ -1,11 +1,26 @@
 namespace Properties.Patches;
 
-internal static class Diff
+/// <summary>
+/// The <c>Diff</c> class provides a means of inferring the changes required to
+/// transform one <see cref="IPropertyDictionary{T}" /> to another.
+/// </summary>
+public static class Diff
 {
-    public static Patch ComputeFor(IEnumerable<Property> one, IEnumerable<Property> two)
+    /// <summary>
+    /// Provides the <see cref="Patch" /> that defines the transformation from the
+    /// <paramref name="first" /> <see cref="IPropertyDictionary{T}" /> to the
+    /// <paramref name="second" />.
+    /// </summary>
+    /// <param name="first">The source <see cref="IPropertyDictionary{T}" />.</param>
+    /// <param name="second">The target <see cref="IPropertyDictionary{T}" />.</param>
+    /// <returns>
+    /// A <see cref="Patch" /> to obtain the <paramref name="second" />
+    /// <see cref="IPropertyDictionary{T}" /> from the <paramref name="first" />.
+    /// </returns>
+    public static Patch ComputeFor(IEnumerable<Property> first, IEnumerable<Property> second)
     {
-        var x = one.ToList();
-        var y = two.ToList();
+        var x = first.ToList();
+        var y = second.ToList();
         return new Patch(
             x
                .Except(y)
